3274cf17bbbea73418cc8b137e6a74b16495bd5a
Nikhil Sharma <nikhilsharma7028@gmail.com>
2024-03-14 01:18:22 +0530

    ToolTip Updates

--- components/Nav.js
+++ components/Nav.js
@@ -43,11 +43,25 @@ const Nav = () => {
                     xl:text-xl xl:rounded-full'>
         {navData.map((link, index) => {
           return (
-            <Link className={`${link.path === pathname && 'text-accent'}`} href={link.path} key={index}>
-                <div>
-                  {link.icon}
+            <Link className={`${link.path === pathname && 'text-accent'} relative flex items-center group
+                            hover:text-accent transition-all duration-300`} href={link.path} key={index}>
+              {/* tooltip */}
+              <div className='absolute pr-14 right-0 hidden xl:group-hover:flex'>
+                <div className='bg-white relative flex text-primary items-center p-[6px] rounded-[3px]'>
+                  <div className='text-[12px] leading-none font-semibold capitalize'>
+                    {link.name}
+                  </div>
+                  {/* triangle */}
+                  <div className='border-solid border-l-white border-l-8 border-y-transparent border-y-[6px]
+                                  border-r-0 absolute -right-2'>
+                  </div>
                 </div>
-              </Link>
+              </div>
+              {/* icon */}
+              <div>
+                {link.icon}
+              </div>
+            </Link>
           );
         })}
       </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import '../styles/globals.css';
+
+// components
 import Layout from '../components/Layout';
+import Transition from '../components/Transition';
+
+// router
+import { useRouter } from 'next/router';
+
+// framer motion 
+import { AnimationPresence, motion} from 'framer-motion';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
   return (
     <Layout>
       <Component {...pageProps} />

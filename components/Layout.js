a400fcf55b5a50b330c1e8a6ac62b50be134e205
Nikhil Sharma <nikhilsharma7028@gmail.com>
2024-03-13 22:24:04 +0530

    First Commit

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,41 @@
-const Layout = () => {
-  return <div>Layout</div>;
+import { Sora } from '@next/font/google';
+import Image from 'next/image';
+
+const sora = Sora({
+  subsets: ['latin'],
+  variable: '--font-sora',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
+});
+
+const Layout = ({ children }) => {
+  const [width, setWidth] = React.useState(200);
+
+  React.useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 1280) {
+        setWidth(400);
+      } else {
+        setWidth(200);
+      }
+    };
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return (
+    <div className={`page bg-site text-white bg-cover bg-np-repeat ${sora.variable} font-sora relative`}>
+      <div className='top-left-img'>
+        <Image src='/public/avatar.png' width={width} height={width} alt='' />
+      </div>
+      {children}
+    </div>
+  );
 };
 
 export default Layout;
--- components/TopLeftImg.js
+++ components/TopLeftImg.js
@@ -1,5 +1,11 @@
-const TopLeftImg = () => {
-  return <div>Top Left Image</div>;
+import Image from 'next/image';
+
+const TopLeftImg = ({ width}) => {
+  return (
+    <div className='top-left-img'>
+    <Image src='/public/top-left-img.png' width={width} height={width} alt='' />
+  </div>
+  );
 };
 
 export default TopLeftImg;

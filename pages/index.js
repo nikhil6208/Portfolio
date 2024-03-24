d09accc6a7b261d2d2a41950f672101fd0cc1c4f
Nikhil Sharma <nikhilsharma7028@gmail.com>
2024-03-25 01:05:51 +0530

    Projects Btn update

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,14 @@
-import { fadeIn} from '../variants';
+import Image from 'next/image';
+
+// components
+import ParticlesContainer from '../components/ParticlesContainer';
+import ProjectsBtn from '../components/ProjectsBtn';
+import Avatar from '../components/Avatar';
+
+// motion
+import {motion} from 'framer-motion';
+
+import { fadeIn } from '../variants';
 
 const Home = () => {
   return (
@@ -7,18 +17,21 @@ const Home = () => {
       <div className='w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10'>
         <div className='text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto'>
           {/* title */}
-          <h1 className='h1'>Tranforming Ideas<br />Into{' '}
+          <motion.h1 variants={fadeIn('down', 0.2)} initial='hidden' animate='show' exit='hidden' className='h1'>Tranforming Ideas<br />Into{' '}
             <span className='text-accent'>Digital Reality</span>
-          </h1>
-          <p className='max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16'>
+          </motion.h1>
+          <motion.p variants={fadeIn('down', 0.3)} initial='hidden' animate='show' exit='hidden' className='max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16'>
             Turning ideas into digital reality involves conceptualization, design, development, 
             and deployment, blending innovation and problem-solving to create tangible digital 
             solutions.
-          </p>
+          </motion.p>
           {/* button */}
-          {/* <div className='flex justify-center xl:hidden relative'>
+          <div className='flex justify-center xl:hidden relative'>
             <ProjectsBtn />
-          </div> */}
+          </div>
+          <motion.div variants={fadeIn('down', 0.4)} initial='hidden' animate='show' exit='hidden' className='hidden xl:flex'>
+            <ProjectsBtn />
+          </motion.div>
         </div>
       </div>
       {/* image */}

5508d280abd28ef9e0e91b30d475a9ab852d6aa6
Nikhil Sharma <nikhilsharma7028@gmail.com>
2024-03-25 00:04:25 +0530

    Index Page Updates

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,5 @@
 const Header = () => {
-  return <header>Header</header>;
+  return <header>LOGO</header>;
 };
 
 export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,30 @@
+import { fadeIn} from '../variants';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className='bg-primary/60 h-full'>
+      {/* text */}
+      <div className='w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10'>
+        <div className='text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto'>
+          {/* title */}
+          <h1 className='h1'>Tranforming Ideas<br />Into{' '}
+            <span className='text-accent'>Digital Reality</span>
+          </h1>
+          <p className='max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16'>
+            Turning ideas into digital reality involves conceptualization, design, development, 
+            and deployment, blending innovation and problem-solving to create tangible digital 
+            solutions.
+          </p>
+          {/* button */}
+          {/* <div className='flex justify-center xl:hidden relative'>
+            <ProjectsBtn />
+          </div> */}
+        </div>
+      </div>
+      {/* image */}
+      <div>Image</div>
+    </div>
+  );
 };
 
 export default Home;
